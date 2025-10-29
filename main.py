4c6238b5028451c09f556ad8519a02c4bd6c8867
wowwow0320 <oon418@gmail.com>
2025-10-29 09:57:23 +0900

    test scheduler 09:58

--- main.py
+++ main.py
@@ -22,8 +22,8 @@ scheduler = AsyncIOScheduler()
 
 @scheduler.scheduled_job(
     CronTrigger(
-        hour="8,14,18,23",  # 요청하신 시간: 오전 8시, 14시, 18시, 23시
-        minute="50",
+        hour="9,14,18,23",  # 요청하신 시간: 오전 8시, 14시, 18시, 23시
+        minute="58",
         timezone="Asia/Seoul" # 한국 시간 기준
     ),
     id="crawl_yongin_notices_job",
